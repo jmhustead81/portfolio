bd7ea07f974852eac74cb87552812ffd5248d230
jmhustead81 <99774888+jmhustead81@users.noreply.github.com>
2025-12-18 20:42:15 -0500

    Responsive updates. Color template updates. Favicon, pic updates.

--- scripts/global.js
+++ scripts/global.js
@@ -1,67 +1,71 @@
 function setFooterYear() {
-    var currentYear = new Date().getFullYear();
+  var currentYear = new Date().getFullYear();
 
-    $('#footerCurrentYear').text(currentYear);
+  $("#footerCurrentYear").text(currentYear);
 }
 
 function smoothScroll() {
-    $('a[href*="#"]:not([href="#"])').click(function () {
-        if (
-            window.location.pathname.replace(/^\//, "") ===
-            this.pathname.replace(/^\//, "") &&
-            window.location.hostname === this.hostname
-        ) {
-            var target = $(this.hash);
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (
+      window.location.pathname.replace(/^\//, "") ===
+        this.pathname.replace(/^\//, "") &&
+      window.location.hostname === this.hostname
+    ) {
+      var target = $(this.hash);
 
-            target = target.length
-                ? target
-                : $("[name=" + this.hash.slice(1) + "]");
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
 
-            if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top,
-                    },
-                    400
-                );
-                return false;
-            }
-        }
-    });
-};
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          400
+        );
+        return false;
+      }
+    }
+  });
+}
 
-function showLightbox(img, label) {
-    $('#lightbox-img').attr('src', img);
-    $('#lightbox-label').text(label);
-    $('#lightbox').fadeIn(400);
+function showLightbox(img, caption) {
+  $("#lightbox-bg").fadeIn(400);
+  $("#lightbox-img").attr("src", img);
+  $("#lightbox-caption").text(caption);
+  $("#lightbox").fadeIn(400);
 }
 
 function hideLightbox() {
-    $('#lightbox').fadeOut(400);
+  $("#lightbox").fadeOut(400);
+  $("#lightbox-bg").fadeOut(400);
 }
 
-$('.projects-item a').on('click', function (event) {
-    event.preventDefault();
+$(".projects-item a").on("click", function (event) {
+  event.preventDefault();
 
-    var imgSrc = $(this).find('img').attr('src');
-    var imgLabel = $(this).find('label').text();
+  var imgSrc = $(this).find("img").attr("src");
+  var imgCaption = $(this).find("figcaption").text();
 
-    if (imgSrc) {
-        showLightbox(imgSrc, imgLabel);
-    }
+  if (imgSrc) {
+    showLightbox(imgSrc, imgCaption);
+  }
 });
 
-$('#lightbox').on('click', function (event) {
+$("#lightbox").on("click", function (event) {
+  hideLightbox();
+});
+
+$("#lightbox-bg").on("click", function (event) {
+  hideLightbox();
+});
+
+$(document).on("keydown", function (e) {
+  if (e.key === "Escape") {
     hideLightbox();
-});
-
-$(document).on('keydown', function (e) {
-    if (e.key === "Escape") {
-        hideLightbox();
-    }
+  }
 });
 
 $(document).ready(function () {
-    setFooterYear();
-    smoothScroll();
+  setFooterYear();
+  smoothScroll();
 });
